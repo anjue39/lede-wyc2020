775592a27bbd9ac328ab490312e10f7088240428
W_Y_CPP <383152993@qq.com>
2022-04-14 02:09:03 -0400

    packages:update default packages

--- include/target.mk
+++ include/target.mk
@@ -48,9 +48,9 @@ DEFAULT_PACKAGES.router:=\
 	ppp-mod-pppoe \
 	luci-newapi block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-inet-diag \
 	default-settings luci luci-app-ddns luci-app-upnp luci-app-autoreboot \
-	luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-unblockmusic luci-app-arpbind \
+	luci-app-filetransfer luci-app-vsftpd \
 	luci-app-vlmcsd luci-app-wol luci-app-ramfree \
-	luci-app-turboacc luci-app-nlbwmon luci-app-accesscontrol ddns-scripts_aliyun ddns-scripts_dnspod
+	luci-app-turboacc luci-app-accesscontrol ddns-scripts_aliyun ddns-scripts_dnspod
 
 ifneq ($(DUMP),)
   all: dumpinfo
